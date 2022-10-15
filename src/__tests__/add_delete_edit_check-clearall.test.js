654953e4653b565656270145b467360755220e60
Juwon <dnladesanya1@gmail.com>
2022-10-15 19:37:57 +0100

    linter fixed

--- src/__tests__/add_delete_edit_check-clearall.test.js
+++ src/__tests__/add_delete_edit_check-clearall.test.js
@@ -4,6 +4,7 @@
 import store from '../modules/store.js';
 import renderList from '../modules/function.js';
 import formClear from '../modules/cleartask.js';
+
 document.body.innerHTML = '<input type="text" name="task" class="text-field full" placeholder="Add to your list..." required> <ul class="task-list"> </ul> <a class="clear-btn" href="">Clear all completed</a>';
 
 describe('Store', () => {
@@ -23,7 +24,6 @@ describe('Delete task', () => {
     expect(listElement).toHaveLength(0);
   });
 });
-
 
 describe('Edit task', () => {
   test('Should edit text when clicked', () => {
