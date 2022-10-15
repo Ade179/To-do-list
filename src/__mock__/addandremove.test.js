91bcebe6cfc5b17f02ef950c14f970838bab0858
Alex Ogeto <alexogeto54@gmail.com>
2022-10-15 15:13:30 +0300

    Update addandremove.test.js

--- src/__mock__/addandremove.test.js
+++ src/__mock__/addandremove.test.js
@@ -5,4 +5,8 @@ describe('should addandremove a task', () => {
     const arr = [2, 3, 5];
     expect(arr.splice(1, 2)).toHaveLength(2);
   });
+  test('checking for task addition',()=>{
+        const arr = [3]
+        expect( arr.length).toBe(1)
+    })
 });
